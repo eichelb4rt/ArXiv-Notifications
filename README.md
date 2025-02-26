5aa57d61ef54270a7ba6f6ec08f7451b34e8748e
eichelb4rt <maurice.wenig@gmail.com>
2025-02-26 17:07:16 +0100

    replaced email pdf with telegram messages

--- README.md
+++ README.md
@@ -1,35 +1,36 @@
 # ArXiV Updates via LLM generated summaries
-This script is meant to search for new papers, summarize them using an LLM and send out a pdf file via email.
-We further provide functionality to let it run once a day on windows.
+
+This script is meant to search for new papers, summarize them using an LLM and send out summaries via a telegram bot.
 
 ## LLM
+
 In this version, we are using the free [Mistral](https://console.mistral.ai/) model.
 If you want to change it, simply provide a different function to the `make_summaries` function.
 
 ## Requirements
+
+```text
+uv venv
+uv pip install -r requirements.txt
 ```
-conda create --name arxiv_update
-conda activate arxiv_update
-pip install -r requirements.txt
-```
 
+## Setup
 
-## Configuration
+### Config
 
+You can edit your configuration in `config.json`.
 
-
-You can set everything you need in the `config.json` file:
 - `keywords`: Keywords to search on ArXiV. The lists represent a disjunktive normal form (DNF). That is, the lists are concatenated with OR, the entries of each list with AND.
 - `preferences`: Your personal preferences for creating the summaries. This will be added to the LLM prompt.
-- `emails`: To whom the emails will be sent.
 - `last_date`: Start date for search (up to today, format YYYY-MM-DD)
-- `smtp_*`: SMTP stuff for sending the mail (see [here](https://realpython.com/python-send-email/) for an example) 
 - `download_dir`: Where papers are downloaded internally
-- `summary_dir`: Where summaries are saved
-- `max_results`: Maximum number of papers per category
+- `max_results`: Maximum number of papers per category. Keep it mind that a standard telegram bot can only send 20 messages per minute.
 - `max_pages`: Maximum number of pages for LLM to read
 
+### Secrets
+
+Put your API tokens and the telegram chat id into `.env`. A template for this can be found in `.env.template`.
+
 ## Scedule running once a day
+
 On Windows, follow the instructions [here](https://mikenguyen.netlify.app/post/task-scheduler-with-python-and-anaconda-environment/) using the `run_updater.bat` file.
-
-
--- arxiv_updater.py
+++ arxiv_updater.py
@@ -1,30 +1,19 @@
-# general
-from datetime import date, datetime
-import os
 import json
+import os
+import urllib.parse
+import urllib.request
+from datetime import date, datetime
+from typing import Any, Callable
+
+import feedparser
+import pymupdf
+import pymupdf4llm
+import requests
+from dotenv import dotenv_values
+from mistralai import Mistral
 from tqdm import tqdm
 
-# Arxiv API
-import urllib.request
-import urllib.parse
-import feedparser
-
-# scrape pdf and feed to LLM
-from mistralai import Mistral
-import pymupdf4llm
-import pymupdf
-
-# create summary pdf
-from pylatex import Command, Document, Section, Package,  NewLine
-from pylatex.utils import NoEscape, escape_latex
-from pylatexenc import latexencode
-
-# sending mail
-import smtplib
-import ssl
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
+SECRETS = dotenv_values(".env")
 
 
 CS_CLASSES = [
@@ -36,7 +25,11 @@ CS_CLASSES = [
     ]
 ]
 
-def query_mistral(query:str) -> str:
+
+ArticleInfo = dict[str, Any]
+
+
+def query_mistral(query: str) -> str:
     '''
     Queries Mistral model.
 
@@ -48,13 +41,11 @@ def query_mistral(query:str) -> str:
     '''
 
     model = "mistral-large-latest"
-    api_key = os.environ["MISTRAL_API_KEY"]
-    client = Mistral(api_key=api_key)
+    client = Mistral(api_key=SECRETS["MISTRAL_API_TOKEN"])
 
-    
     chat_response = client.chat.complete(
-        model = model,
-        messages = [
+        model=model,
+        messages=[
             {
                 "role": "user",
                 "content": query
@@ -64,7 +55,8 @@ def query_mistral(query:str) -> str:
     outp = chat_response.choices[0].message.content
     return outp
 
-def query_arxiv(keywords : list, last_date : str, max_results : int) -> dict:
+
+def query_arxiv(keywords: list[str], last_date: str, max_results: int) -> dict[str, ArticleInfo]:
     '''
     First step of pipeline: Search for new papers on ArXiV.
 
@@ -88,35 +80,32 @@ def query_arxiv(keywords : list, last_date : str, max_results : int) -> dict:
             tmp += '%22'
             kw_str += f'{tmp}+AND+'
         kw_str = kw_str[:-5]
-        
+
         kw_query += f'{kw_str}%29+OR+'
     kw_query = kw_query[:-4]
     kw_query += '%29'
 
-    
     articles = {}
-    counter = 0
     for cat in CS_CLASSES:
         query = f'http://export.arxiv.org/api/query?search_query={kw_query}+AND+cat:{cat}&start=0&max_results={max_results}&sortBy=lastUpdatedDate&sortOrder=descending'
         response = urllib.request.urlopen(query).read()
         feed = feedparser.parse(response)
         if len(feed.entries) > 0:
             for i, article in enumerate(feed.entries):
-                if(article['updated_parsed'] >= start_date.timetuple()):
+                if (article['updated_parsed'] >= start_date.timetuple()):
                     article_info = {
-                        'title' : article['title'].replace('\n', ''),
-                        'date' : article['updated_parsed'],
-                        'authors' : [author['name'] for author in article['authors']],
-                        'link' : article['link'],
-                        'abstract' : article['summary'].replace('\n', '')
+                        'title': article['title'].replace('\n', ''),
+                        'date': article['updated_parsed'],
+                        'authors': [author['name'] for author in article['authors']],
+                        'link': article['link'],
+                        'abstract': article['summary'].replace('\n', '')
                     }
-                    id = article_info['title'].replace(' ', '_').replace(':', '_').replace('-', '_').replace('__', '_').replace('___', '_').replace('?', '').replace('!', '').lower()
-                    if id not in articles:
-                        counter += 1
-                    articles[id] = article_info
+                    article_id = article_info['title'].replace(' ', '_').replace(':', '_').replace('-', '_').replace('__', '_').replace('___', '_').replace('?', '').replace('!', '').lower()
+                    articles[article_id] = article_info
     return articles
 
-def download_articles(articles: dict, download_dir:str):
+
+def download_articles(articles: dict[str, ArticleInfo], download_dir: str) -> None:
     '''
     Second step of pipeline: download the papers.
 
@@ -136,7 +125,8 @@ def download_articles(articles: dict, download_dir:str):
     for filename in del_list:
         del articles[filename]
 
-def make_summaries(download_dir:str, preferences:list, query_llm:callable = query_mistral) -> dict:
+
+def make_summaries(download_dir: str, preferences: list[str], max_pages: int, query_llm: Callable[[str], str] = query_mistral) -> dict[str, str]:
     '''
     Third step of pipeline: scrape text from pdf and use LLM to summarize.
 
@@ -160,17 +150,17 @@ def make_summaries(download_dir:str, preferences:list, query_llm:callable = quer
     query += "Do not repeat the title and the authors of the paper.\n"
     if len(preferences) > 0:
         query += "Keep in mind what might be interesting for the researcher regarding his preferences.\n"
-    
-    
-    outps = {}
+
+    summaries = {}
     for filename in tqdm(os.listdir(download_dir)):
         filepath = os.path.join(download_dir, f'{filename}')
         doc = pymupdf.open(filepath)
-        md_text = pymupdf4llm.to_markdown(filepath, pages = range(min(max_pages, len(doc))), show_progress= False)
-        outps[filename] = query_llm(query + md_text)
-    return outps
+        md_text = pymupdf4llm.to_markdown(filepath, pages=range(min(max_pages, len(doc))), show_progress=False)
+        summaries[filename] = query_llm(query + md_text)
+    return summaries
 
-def create_summary_pdf(articles:dict, summaries:dict, keywords:list, summary_dir:str, timestamp:datetime.date) -> str:
+
+def create_overviews(articles: dict[str, ArticleInfo], summaries: dict[str, str], keywords: list[list[str]]) -> list[str]:
     '''
     Fourth step of pipeline:Creates a single pdf as summary.
 
@@ -184,118 +174,44 @@ def create_summary_pdf(articles:dict, summaries:dict, keywords:list, summary_dir
     @Returns:
         path to file
     '''
-    
+
     # sort by date
     filenames = list(summaries.keys())
     dates = [f"{datetime(*articles[filename[:-4]]['date'][:6]):%Y-%m-%d}" for filename in filenames]
     filenames = [x for _, x in sorted(zip(dates, filenames))]
 
-
     replace_dict = {
-        '𝜖' : 'epsilon'
+        '𝜖': 'epsilon'
     }
-    datestr = "{:%B %d, %Y}".format(datetime.now())
-    
-    doc = Document()
-    doc.packages.append(Package('hyperref'))
-    
-    title = 'New Papers for'
-    for kw in keywords:
-        line_str = '('
-        for tmp in kw:
-            line_str += f' {tmp} AND'
-        line_str = line_str[:-4] + ' )'
-        title += f' {line_str},'
-    title = title[:-1]
-    
-    doc.preamble.append(Command("title", title))
-    doc.preamble.append(Command("date", NoEscape(r"\today")))
-    doc.append(NoEscape(r"\maketitle"))
-    
+
+    overviews = []
     for filename in filenames:
-        paper_title = latexencode.unicode_to_latex(articles[filename[:-4]]['title'])
-        splits = paper_title.split('\\$')
-        
-        for i in range(len(splits)):
-            if i%2 == 1:
-                splits[i] = f'\\texorpdfstring{{${splits[i]}$}}{{{splits[i]}}}'
-        paper_title = '\\boldmath '
-        for s in splits:
-            paper_title += s
-        with doc.create(Section(NoEscape(paper_title))):
-            
-            text = summaries[filename]
-            for c in replace_dict:
-                text = text.replace(c, replace_dict[c])
-        
-            doc.append(latexencode.unicode_to_latex(text))
-            #doc.append(NoEscape(outps[filename]))
-            doc.append(NewLine())
-            tmp = f"{datetime(*articles[filename[:-4]]['date'][:6]):%Y-%m-%d}"
-            date_str = f"Date: {tmp}, "
-            doc.append(date_str)
-            link = escape_latex(articles[filename[:-4]]['link'])
-            link_str = f"Link: \\url{{{link}}}"
-            doc.append(NoEscape(link_str))
+        overview = f"<b>Title:</b> {articles[filename[:-4]]["title"]}\n\n"
+        summary = summaries[filename]
+        for c in replace_dict:
+            summary = summary.replace(c, replace_dict[c])
+        overview += f"<b>Summary:</b>\n{summary}\n\n"
+        overview += f"<b>Authors:</b> {", ".join(articles[filename[:-4]]["authors"])}\n"
+        overview += f"<b>Date:</b> {datetime(*articles[filename[:-4]]['date'][:6]):%Y-%m-%d}\n"
+        overview += f"<b>Link:</b> {articles[filename[:-4]]["link"]}"
+        overviews.append(overview)
+    return overviews
 
 
-    
-    summary_name = f'summary_{timestamp:%Y_%m_%d}'
-    doc.generate_pdf(os.path.join(summary_dir, summary_name), clean_tex=False, compiler='pdfLaTeX')
-    
-    
-    path_to_file = os.path.join(summary_dir, f'{summary_name}.pdf')
-    return path_to_file
+def send_message(message: str, chat_id: int) -> None:
+    args = {
+        "parse_mode": "HTML",
+        "chat_id": chat_id,
+        "text": message,
+        "link_preview_options": json.dumps({"is_disabled": True}),
+    }
+    response = requests.post(f"https://api.telegram.org/bot{SECRETS["TELEGRAM_API_TOKEN"]}/sendMessage?{urllib.parse.urlencode(args)}")
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        print(response.text)
 
-def send_emails(emails:list, smtp_server:str, smtp_port:str, smtp_login:str, smtp_pw:str, path_to_file:str, keywords:list, last_date:str, timestamp:datetime.date, n_papers:int):
-    '''
-    Last step of pipeline: sends email with summary results.
 
-    @Params:
-        emails... list of emails to send to
-        smtp_server... provider of smtp
-        smtp_port... port of smtp
-        smtp_login... account from where to send
-        smtp_pw... password for account
-        path_to_file... path to file for attachment
-        keywords... list of keywords that were used for search
-        last_date... start date for search
-        timestamp... end date for search
-        n_papers... how many papers were found
-    '''
-    
-    subject = "ArXiV update"
-    body = f"We found {n_papers} new papers regarding the following topics:\n"
-    for kw in keywords:
-        line_str = '- '
-        for tmp in kw:
-            line_str += f'{tmp}, '
-        line_str = line_str[:-2]
-        body += f'{line_str}\n'
-    body += f'Time period: {last_date} until {timestamp:%Y-%m-%d}\n'
-    body += f"See the summary in the attachment."
-    
-    for recipient_email in emails:
-        message = MIMEMultipart()
-        message['Subject'] = subject
-        message['From'] = smtp_login
-        message['To'] = recipient_email
-        body_part = MIMEText(body)
-        message.attach(body_part)
-    
-        with open(path_to_file,'rb') as file:
-            message.attach(MIMEApplication(file.read(), Name=f'summary_{timestamp:%Y_%m_%d}.pdf'))
-    
-        context = ssl.create_default_context()
-        with smtplib.SMTP(smtp_server, smtp_port, timeout=5) as server:
-            server.ehlo()
-            server.starttls(context=context)
-            server.ehlo()
-            server.login(smtp_login, smtp_pw)
-            server.sendmail(smtp_login, recipient_email, message.as_string())
-    
-
-if __name__ == '__main__':
+def main():
     # read from config file
     cwd = os.path.dirname(__file__)
     with open(os.path.join(cwd, 'config.json')) as f:
@@ -303,68 +219,58 @@ if __name__ == '__main__':
     keywords = config['keywords']
     preferences = config['preferences']
     last_date = config['last_date']
-    emails = config['emails']
-    smtp_server = config['smtp_server']
-    smtp_port = config['smtp_port']
-    smtp_login = config['smtp_login']
-    smtp_pw = config['smtp_pw']
     download_dir = os.path.join(cwd, config['download_dir'])
-    summary_dir = os.path.join(cwd, config['summary_dir'])
     max_results = config['max_results']
     max_pages = config['max_pages']
+    chat_id = SECRETS["TELEGRAM_CHAT_ID"]
     ###################################
-
 
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
-    if not os.path.exists(summary_dir):
-        os.makedirs(summary_dir)
 
     print('Scraping ArXiV...')
     articles = query_arxiv(keywords, last_date, max_results)
     print('done.')
     timestamp = datetime.now()
-
-    if len(articles) > 0:
-        print(f'Found {len(articles)} new articles.')
-        
-        print('Downloading PDFs...')
-        download_articles(articles, download_dir)
-        print('done.')
-
-        print('Using LLM to make summaries...')
-        summaries = make_summaries(download_dir, preferences)
-        print('done.')
-
-        print('Creating PDF...')
-        try:
-            path_to_file = create_summary_pdf(articles, summaries, keywords, summary_dir, timestamp)
-        except:
-            print('Error occured, but PDF was generated nonetheless')
-            summary_name = f'summary_{timestamp:%Y_%m_%d}'
-            for file in os.listdir(summary_dir):
-                if not file.endswith('pdf'):
-                    p = os.path.join(summary_dir, file)
-                    os.remove(p)
-            path_to_file = os.path.join(summary_dir, f'{summary_name}.pdf')
-            assert os.path.exists(path_to_file)
-        print(f'done.\nPDF located at {path_to_file}')
-
-        print('Sending emails...')
-        send_emails(emails, smtp_server, smtp_port, smtp_login, smtp_pw, path_to_file, keywords, last_date, timestamp, len(articles))
-        print('done.')
-
-        print('Deleting articles...')
-        for file in os.listdir(download_dir):
-            os.remove(os.path.join(download_dir, file))
-        print('done.')
-            
-
-    else:
+    
+    if len(articles) == 0:
         print('No new articles found.')
+        return
+
+    
+    print(f'Found {len(articles)} new articles.')
+
+    print('Downloading PDFs...')
+    download_articles(articles, download_dir)
+    print('done.')
+
+    print('Using LLM to make summaries...')
+    summaries = make_summaries(download_dir, preferences, max_pages)
+    print('done.')
+
+    print('Creating PDF...')
+    overviews = create_overviews(articles, summaries, keywords)
+
+    print('Sending message...')
+    initial_message = f"Found {len(overviews)} new Papers for {", ".join(
+        [f"({" & ".join(kw_list)})" for kw_list in keywords]
+    )}"
+    send_message(initial_message, chat_id)
+    for overview in overviews:
+        send_message(overview, chat_id)
+    print('done.')
+
+    print('Deleting articles...')
+    for file in os.listdir(download_dir):
+        os.remove(os.path.join(download_dir, file))
+    print('done.')
 
     print('Updating config...')
     config['last_date'] = f'{timestamp:%Y-%m-%d}'
-    with open(os.path.join(cwd, 'config.json'),'w') as outfile:
-        json.dump(config, outfile, indent = '\t')
+    with open(os.path.join(cwd, 'config.json'), 'w') as outfile:
+        json.dump(config, outfile, indent='\t')
     print('done.')
+
+
+if __name__ == "__main__":
+    main()
